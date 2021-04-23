e1f72f627cdf613f1d8986477cbcd46b3e4bcf67
Jefferey Fitzpatrick <jeffereyfitzpatrick@Jeffereys-MBP.lan>
2021-04-22 22:07:49 -0400

    Day 2 done

--- api/index.js
+++ api/index.js
@@ -1,15 +1,6 @@
 const express = require("express");
 const apiRouter = express.Router();
 
-const usersRouter = require("./users");
-apiRouter.use("/users", usersRouter);
-
-const postsRouter = require("./posts");
-apiRouter.use("/posts", postsRouter);
-
-const tagsRouter = require("./tags");
-apiRouter.use("/tags", tagsRouter);
-
 const jwt = require("jsonwebtoken");
 const { getUserById } = require("../db");
 const { JWT_SECRET } = process.env;
@@ -48,6 +39,15 @@ apiRouter.use((req, res, next) => {
   
     next();
   });
+
+  const usersRouter = require("./users");
+apiRouter.use("/users", usersRouter);
+
+const postsRouter = require("./posts");
+apiRouter.use("/posts", postsRouter);
+
+const tagsRouter = require("./tags");
+apiRouter.use("/tags", tagsRouter);
 
 apiRouter.use((error, req, res, next) => {
   res.send(error);
